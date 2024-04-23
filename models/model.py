a7e17e9b6da15729890de4f72e247701f3459ef6
WeThinkCode_ <wethinkcode_@pop-os.localdomain>
2024-04-23 14:40:11 +0200

    added crud operations

--- models/model.py
+++ models/model.py
@@ -2,35 +2,44 @@ import pymongo
 from dotenv import load_dotenv
 import os
 
-myclient = pymongo.MongoClient
+
+
+
+
+
 
 
 load_dotenv()
 
 try:
     mongodb_uri = os.getenv("MONGO_URI")
+    client = pymongo.MongoClient(mongodb_uri)
     print("Succesfully connected to database")
+
 except:
     print("Something went wrong whilest establishing the connection")
 
 
     
+
+db = client.get_database("family_feud")
+users = db.get_collection("users")
+
+
+def save_user(user:dict, users):
+    saved_user = users.insert_one(user)
+    return saved_user
+
+
+def delete_user(user, users):
+    users.delete_one(user)
+
+
+def update_user(user, users):
+    users.update_one(user)
+
     
 
 
-client = pymongo.MongoClient(mongodb_uri)
 
-db = client.get_database("family_feud")
-
-# collection_names = db.get_collection("users")
-users = db.get_collection("users")
-
-# all_users = users.find({"name": "Ned Stark"})
-
-# for user in all_users:
-#     print(user)
-
-# print("Collections in the user Database")
-# for collection_name in collection_names:
-#     print(collection_name)
 
